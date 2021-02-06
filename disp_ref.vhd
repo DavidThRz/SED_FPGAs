7219350f28b6581ae65763326df1f07016e3babb
WenqiangWang98 <36535022+WenqiangWang98@users.noreply.github.com>
2021-02-06 14:20:55 +0100

    Update disp_ref.vhd

--- disp_ref.vhd
+++ disp_ref.vhd
@@ -3,40 +3,39 @@ use IEEE.std_logic_1164.all;
 
 entity disp_ref is
 	port(
-    	CLK			: in std_logic;                     --Reloj de entrada
-        ce 			: in std_logic;                     --Hay que mostrar o no
-        segment_u   : in std_logic_vector(6 downto 0);  --Segmento unidades
-        segment_d   : in std_logic_vector(6 downto 0);  --Segmento decenas
-        disp_num 	: out std_logic_vector(6 downto 0); --Numero a mostrar en el display
-        disp_selec  : out std_logic_vector(7 downto 0)  --Selección del display
+    	CLK			: in std_logic;                 --Reloj de entrada
+        ce 			: in std_logic;                 --Hay que mostrar o no
+        segment_u   : in std_logic_vector(6 downto 0);  	--Segmento unidades
+        segment_d   : in std_logic_vector(6 downto 0);  	--Segmento decenas
+        disp_num 	: out std_logic_vector(6 downto 0); 	--Numero a mostrar en el display
+        disp_selec  : out std_logic_vector(7 downto 0)  	--SelecciÃ³n del display
     );
 end disp_ref;
 
 architecture behavioral of disp_ref is
 
-    signal count            : integer := 0;
-    signal clk_divisor      : std_logic;    --1 segundo de ciclo
-	signal aux              : std_logic := '1'; 
+   	signal count            : integer := 0; 	--contador iniciado a 0
+	signal clk_divisor      : std_logic;    	--1 segundo de ciclo
+	signal aux              : std_logic := '1'; 	--signal auxiliar iniciado a nivel alto
 
-begin
-
-    process(CLK) 
 	begin
-    	
-    	if(CLK'event and CLK = '1') then
+
+    		process(CLK) 
+			begin
+    				if(CLK'event and CLK = '1') then
 			
-	        count <=count+1;
-		  	if(count = 2 - 1) then   -----Poner 10 - 1
-			    aux <= not aux;
-			    count <=0;
-		    end if;
-        end if;
+	        			count <=count+1;
+		  			if(count = 2 - 1) then  --Poner la division, en este caso 2, unidad y decenas.
+			    		aux <= not aux;	--Alterar la senal
+			    		count <=0;		--resetear la senial
+		    			end if;
+        			end if;
        
-	end process;
+		end process;
     
 	
-    process(ce,segment_u,segment_d,clk_divisor)
-    begin
+    	process(ce,segment_u,segment_d,clk_divisor)	--alternar la salida dependiendo del numero a mostrar
+   	begin
     	if ce ='0' then
         	disp_selec<="11111111";
         elsif clk_divisor = '1' then
@@ -48,6 +47,6 @@ begin
         end if;
     end process;
     
-    clk_divisor <= aux;
+    clk_divisor <= aux;					--asignar la senal auxiliar al clk deseado
     
 end behavioral;
